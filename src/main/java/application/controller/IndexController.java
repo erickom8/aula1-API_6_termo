975a701d8d7cbdb4da3ff1a9e9183adba142af51
Erick Cirico <126480958+erickom8@users.noreply.github.com>
2025-08-12 19:08:35 +0000

    Feat: Salvando primeiras alterações: testes em http; controlador: GET e POST

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,12 +1,29 @@
 package application.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class IndexController {
-    @RequestMapping("/")
-    public String home() {
-        return "home";
+@RestController
+@RequestMapping("/")
+public class IndexController{
+    // private List<String> frutas = Arrays.asList("Maça", "Melão", "Maracujá");
+    
+    private List<String> frutas = new ArrayList<String>();
+
+    @GetMapping
+    public List<String> home() {
+        return frutas;
+    }
+
+    @PostMapping
+    public List<String> insert(@RequestBody String fruta) {
+        frutas.add(fruta);
+        return frutas;
     }
 }
