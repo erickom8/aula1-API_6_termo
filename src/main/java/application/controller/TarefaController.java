f07f71cf0c42e63c07506358d18def22f1edacf7
Erick Cirico <126480958+erickom8@users.noreply.github.com>
2025-08-19 16:22:38 -0300

    Update TarefaController.java

--- src/main/java/application/controller/TarefaController.java
+++ src/main/java/application/controller/TarefaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Tarefa;
 import application.repository.TarefaRepository;
@@ -27,6 +29,18 @@ public class TarefaController {
         return tarefaRepo.save(novaTarefa);
     }
 
+    @GetMapping(("/{id}"))
+    public Tarefa getOne(@PathVariable long id){
+        Optional<Tarefa> resultado = tarefaRepo.findById(id); 
+
+        if (resultado.isEmpty()){
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Tarefa não encontrada"
+            );
+        }
+        return resultado.get();
+    }
+
     @GetMapping
     public List<Tarefa> getAll() {
         return tarefaRepo.findAll();
@@ -36,16 +50,29 @@ public class TarefaController {
     public Tarefa update(@PathVariable long id, @RequestBody Tarefa novosDados) {
         Optional<Tarefa> resultado = tarefaRepo.findById(id);
         
-        if(resultado.isPresent()) {
-            resultado.get().setDescricao(novosDados.getDescricao());
-            return tarefaRepo.save(resultado.get());
+        if(resultado.isEmpty()){
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Tarefa não encontrada"
+            );
         }
+        
+        resultado.get().setDescricao(novosDados.getDescricao());
+        return tarefaRepo.save(resultado.get());
+        
 
-        return new Tarefa();
     }
 
     @DeleteMapping("/{id}")
     public void remove(@PathVariable long id) {
+        if(!tarefaRepo.existsById(id)) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Tarefa não encontrada"
+            );
+        }
+        
         tarefaRepo.deleteById(id);
+
     }
+
+
 }
